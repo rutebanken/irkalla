0b49703ec0197736f6cda175463b3cf5011d1944
erlendnils1 <erlendnils1@gmail.com>
2017-05-24 09:26:16 +0200

    Trigger chouette sync when effective stop place change is received from Tiamat

--- src/main/java/org/rutebanken/irkalla/routes/tiamat/TiamatStopPlaceChangedRouteBuilder.java
+++ src/main/java/org/rutebanken/irkalla/routes/tiamat/TiamatStopPlaceChangedRouteBuilder.java
@@ -1,18 +1,20 @@
 package org.rutebanken.irkalla.routes.tiamat;
 
 
+import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.rutebanken.irkalla.Constants;
 import org.rutebanken.irkalla.routes.BaseRouteBuilder;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+
 /**
  * Process changelog events for stop places:
- *
+ * <p>
  * * Look up current and, if relevant, previous version of stop
  * * Analyze change
  * * Build and send CRUD event to event handler (Nabu).
- *
  */
 @Component
 public class TiamatStopPlaceChangedRouteBuilder extends BaseRouteBuilder {
@@ -25,9 +27,12 @@ public class TiamatStopPlaceChangedRouteBuilder extends BaseRouteBuilder {
                 .bean("stopPlaceDao", "getStopPlaceChange")
                 .choice()
                 .when(body().isNotNull())
+                .process(e -> e.getIn().setHeader("isEffective", isChangeEffective(e)))
+
                 .bean("stopPlaceChangedToEvent", "toEvent")
                 .convertBodyTo(String.class)
                 .to("activemq:queue:CrudEventQueue")
+                .to("direct:triggerStopPlaceSyncIfChangeIsEffective")
                 .otherwise()
                 .log(LoggingLevel.WARN, "Discarding stop place changed event for unknown stop place:" +
                                                 " ${header." + Constants.HEADER_ENTITY_ID + "} " +
@@ -35,6 +40,17 @@ public class TiamatStopPlaceChangedRouteBuilder extends BaseRouteBuilder {
                 .end()
                 .routeId("tiamat-stop-place-changed");
 
+        from("direct:triggerStopPlaceSyncIfChangeIsEffective")
+                .choice()
+                .when(simple("${header.isEffective}"))
+                .setBody(constant(null))
+                .to("activemq:queue:ChouetteStopPlaceSyncQueue")
+                .routeId("tiamat-trigger-chouette-update-for-changed-stop");
+    }
+
+    private boolean isChangeEffective(Exchange e) {
+        Instant changeTime = e.getIn().getBody(StopPlaceChange.class).getChangeTime();
+        return changeTime != null && changeTime.isBefore(Instant.now());
     }
 
 
