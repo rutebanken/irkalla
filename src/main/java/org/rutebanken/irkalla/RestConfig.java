2a1314a018052ca763daf0b848507da0b53aaf66
erlendnils1 <erlendnils1@gmail.com>
2017-08-21 20:36:45 +0200

    NRP-2001 Removed index page

--- src/main/java/org/rutebanken/irkalla/RestConfig.java
+++ src/main/java/org/rutebanken/irkalla/RestConfig.java
@@ -2,10 +2,8 @@ package org.rutebanken.irkalla;
 
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature;
 import org.rutebanken.irkalla.filter.CorsResponseFilter;
 import org.rutebanken.irkalla.rest.HealthResource;
-import org.rutebanken.irkalla.rest.IndexPageResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -18,23 +16,12 @@ public class RestConfig extends ResourceConfig {
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
     public RestConfig() {
-        registerProperties();
         registerEndpoints();
-    }
-
-    private void registerProperties() {
-        String templatePath = System.getProperty( "template.path" );
-        if ( templatePath != null ) {
-            log.info("Overriding TEMPLATE_BASE_PATH with "+templatePath );
-            property(FreemarkerMvcFeature.TEMPLATE_BASE_PATH, templatePath );
-        }
     }
 
     private void registerEndpoints() {
         register(CorsResponseFilter.class);
         register(LoggingFeature.class);
-        register(FreemarkerMvcFeature.class);
-        register(IndexPageResource.class);
         register(HealthResource.class);
     }
 }
