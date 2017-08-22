1d8ca9add8e2f06928b080c35042976c8b9246d8
erlendnils1 <erlendnils1@gmail.com>
2017-08-22 10:51:03 +0200

    NRP-2001 Added token authorization with keycloak adapter

---
+++ src/main/java/org/rutebanken/irkalla/config/IrkallaSecurityConfiguration.java
@@ -0,0 +1,101 @@
+package org.rutebanken.irkalla.config;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter.AUTHORIZATION_HEADER;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+public class IrkallaSecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(IrkallaSecurityConfiguration.class);
+
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication
+     * manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+
+    /**
+     * Override KeycloakAuthenticationProcessingFilter to support token as query param.
+     *
+     * Must:
+     * - Add request matcher to enable the filter for requests that contain 'access_token' query parameter
+     * - Treat such requests as those containing token as Authorization header. Thus is (somewhat dirty) achieved by overriding method
+     * for checking whether request is of bearer token type to include query param token requests.
+     *
+     */
+    @Override
+    @Bean
+    protected KeycloakAuthenticationProcessingFilter keycloakAuthenticationProcessingFilter() throws Exception {
+        final RegexRequestMatcher tokenQueryParamMatcher = new RegexRequestMatcher("(.*?)access_token=(.*?)", null);
+        RequestMatcher requestMatcher =
+                new OrRequestMatcher(new RequestHeaderRequestMatcher(AUTHORIZATION_HEADER), tokenQueryParamMatcher);
+
+        KeycloakAuthenticationProcessingFilter filter = new KeycloakAuthenticationProcessingFilter(authenticationManagerBean(), requestMatcher) {
+            @Override
+            protected boolean isBearerTokenRequest(HttpServletRequest request) {
+                return super.isBearerTokenRequest(request) || tokenQueryParamMatcher.matches(request);
+            }
+        };
+
+        filter.setSessionAuthenticationStrategy(sessionAuthenticationStrategy());
+        return filter;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        logger.info("Configuring HttpSecurity");
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll();
+    }
+}
--- src/main/java/org/rutebanken/irkalla/routes/AdminRestRouteBuilder.java
+++ src/main/java/org/rutebanken/irkalla/routes/AdminRestRouteBuilder.java
@@ -1,9 +1,16 @@
 package org.rutebanken.irkalla.routes;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.model.rest.RestPropertyDefinition;
+import org.rutebanken.helper.organisation.AuthorizationConstants;
+import org.rutebanken.helper.organisation.NotAuthenticatedException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Collections;
 
@@ -18,9 +25,25 @@ public class AdminRestRouteBuilder extends BaseRouteBuilder {
     @Value("${server.admin.host}")
     public String host;
 
+    @Value("${authorization.enabled:true}")
+    protected boolean authorizationEnabled;
+
+
     @Override
     public void configure() throws Exception {
         super.configure();
+
+        onException(AccessDeniedException.class)
+                .handled(true)
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(403))
+                .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+                .transform(exceptionMessage());
+
+        onException(NotAuthenticatedException.class)
+                .handled(true)
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(401))
+                .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+                .transform(exceptionMessage());
 
         RestPropertyDefinition corsAllowedHeaders = new RestPropertyDefinition();
         corsAllowedHeaders.setKey("Access-Control-Allow-Headers");
@@ -31,22 +54,35 @@ public class AdminRestRouteBuilder extends BaseRouteBuilder {
         restConfiguration()
                 .component("jetty")
                 .bindingMode(RestBindingMode.json)
+                .endpointProperty("filtersRef", "keycloakPreAuthActionsFilter,keycloakAuthenticationProcessingFilter")
+                .endpointProperty("sessionSupport", "true")
+                .endpointProperty("matchOnUriPrefix", "true")
                 .enableCORS(true)
                 .dataFormatProperty("prettyPrint", "true")
                 .host(host)
                 .port(port)
-                .contextPath("/services")
-                .apiContextPath("/api-doc")
+                .contextPath("")
+                .apiContextPath("services/api-doc")
                 .apiProperty("api.title", "Stop place synchronization timetable API")
                 .apiProperty("api.description", "Administration of process for synchronizing stop places in the timetable database (Chouette) with the master data in the stop place registry (NSR)")
                 .apiProperty("api.version", "1.0");
 
-        rest("/stop_place_synchronization_timetable")
+        rest("")
+                .apiDocs(false)
+                .description("Wildcard definitions necessary to get Jetty to match authorization filters to endpoints with path params")
+                .get().route().routeId("admin-route-authorize-get").process(e -> authorize(AuthorizationConstants.ROLE_ROUTE_DATA_ADMIN)).endRest()
+                .post().route().routeId("admin-route-authorize-post").process(e -> authorize(AuthorizationConstants.ROLE_ROUTE_DATA_ADMIN)).endRest()
+                .put().route().routeId("admin-route-authorize-put").process(e -> authorize(AuthorizationConstants.ROLE_ROUTE_DATA_ADMIN)).endRest()
+                .delete().route().routeId("admin-route-authorize-delete").process(e -> authorize(AuthorizationConstants.ROLE_ROUTE_DATA_ADMIN)).endRest();
+
+
+        rest("/services//stop_place_synchronization_timetable")
                 .post("/delta")
                 .description("Synchronize new changes for stop places from Tiamat to Chouette")
                 .responseMessage().code(200).endResponseMessage()
                 .responseMessage().code(500).message("Internal error").endResponseMessage()
                 .route().routeId("admin-chouette-synchronize-stop-places-delta")
+                .process(e -> authorize(AuthorizationConstants.ROLE_ROUTE_DATA_ADMIN))
                 .removeHeaders("CamelHttp*")
                 .inOnly("activemq:queue:ChouetteStopPlaceSyncQueue")
                 .setBody(constant(null))
@@ -56,11 +92,32 @@ public class AdminRestRouteBuilder extends BaseRouteBuilder {
                 .responseMessage().code(200).endResponseMessage()
                 .responseMessage().code(500).message("Internal error").endResponseMessage()
                 .route().routeId("admin-chouette-synchronize-stop-places-full")
+                .process(e -> authorize(AuthorizationConstants.ROLE_ROUTE_DATA_ADMIN))
                 .removeHeaders("CamelHttp*")
                 .setHeader(HEADER_FULL_SYNC, constant(true))
                 .inOnly("activemq:queue:ChouetteStopPlaceSyncQueue")
                 .setBody(constant(null))
                 .endRest();
 
+
+    }
+
+    private void authorize(String requiredRole) {
+        if (!authorizationEnabled) {
+            return;
+        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            throw new NotAuthenticatedException();
+        }
+
+        boolean authorized = false;
+        if (!CollectionUtils.isEmpty(authentication.getAuthorities())) {
+            authorized = authentication.getAuthorities().stream().anyMatch(authority -> requiredRole.equals(authority.getAuthority()));
+        }
+
+        if (!authorized) {
+            throw new AccessDeniedException("Insufficient privileges for operation");
+        }
     }
 }
