a553f09f2ab95de7ee053ba1e08a079c3a36c696
vincent <vincent.paturet@entur.org>
2021-03-09 13:02:12 +0100

    Update Actuator config

--- src/main/java/org/rutebanken/irkalla/RestConfig.java
+++ src/main/java/org/rutebanken/irkalla/RestConfig.java
@@ -18,7 +18,6 @@ package org.rutebanken.irkalla;
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.rutebanken.irkalla.filter.CorsResponseFilter;
-import org.rutebanken.irkalla.rest.HealthResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -37,6 +36,5 @@ public class RestConfig extends ResourceConfig {
     private void registerEndpoints() {
         register(CorsResponseFilter.class);
         register(LoggingFeature.class);
-        register(HealthResource.class);
     }
 }
