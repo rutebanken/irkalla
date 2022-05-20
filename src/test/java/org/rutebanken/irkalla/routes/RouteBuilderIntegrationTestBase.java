39b53862a3d4eeffa10388f7f9254d1aa8891d93
Assad Riaz <assad.riaz@entur.org>
2022-05-20 15:34:32 +0200

    Moves DirtiesContext in RouteBuilderIntegrationTestBase

--- src/test/java/org/rutebanken/irkalla/routes/RouteBuilderIntegrationTestBase.java
+++ src/test/java/org/rutebanken/irkalla/routes/RouteBuilderIntegrationTestBase.java
@@ -19,11 +19,13 @@ import org.apache.camel.model.ModelCamelContext;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.apache.camel.test.spring.junit5.UseAdviceWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @CamelSpringBootTest
 @UseAdviceWith
 @ActiveProfiles({"default",  "in-memory-blobstore", "google-pubsub-emulator", "test"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public abstract class RouteBuilderIntegrationTestBase {
 
     @Autowired
