87cf22eb5627d3a7704e4c4bf9aa288d95e58eb8
Assad Riaz <assad.riaz@entur.org>
2023-04-18 14:12:02 +0200

    terraform config update

--- terraform-gcp2/main.tf
+++ terraform-gcp2/main.tf
@@ -1,42 +1,42 @@
 # Contains main description of bulk of terraform?
 terraform {
   required_version = ">= 0.13.2"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.39"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.13.1"
+    }
+  }
 }
 
-provider "google" {
-  version = ">= 4.26"
-}
-provider "kubernetes" {
-  version = ">= 2.13.1"
-}
-
-
-
-# TODO: Create storage bucket
 # Create bucket
-#resource "google_storage_bucket" "storage_bucket" {
-#  name               = "${var.bucket_instance_prefix}-${var.bucket_instance_suffix}"
-#  force_destroy      = var.force_destroy
-#  location           = var.location
-#  project            = var.storage_project
-#  storage_class      = var.storage_class
-#  labels             = var.labels
-#  uniform_bucket_level_access = true
-#  versioning {
-#    enabled = var.versioning
-#  }
-#  logging {
-#    log_bucket        = var.log_bucket
-#    log_object_prefix = "${var.bucket_instance_prefix}-${var.bucket_instance_suffix}"
-#  }
-#}
+resource "google_storage_bucket" "storage_bucket" {
+  name               = "${var.bucket_instance_prefix}-${var.bucket_instance_suffix}"
+  force_destroy      = var.force_destroy
+  location           = var.location
+  project            = var.storage_project
+  storage_class      = var.storage_class
+  labels             = var.labels
+  uniform_bucket_level_access = true
+  versioning {
+    enabled = var.versioning
+  }
+  logging {
+    log_bucket        = var.log_bucket
+    log_object_prefix = "${var.bucket_instance_prefix}-${var.bucket_instance_suffix}"
+  }
+}
 
 # Create folder in a bucket
-#resource "google_storage_bucket_object" "content_folder" {
-#  name          = "StopPlace/"
-#  content       = "Not really a directory, but it's empty."
-#  bucket        = google_storage_bucket.storage_bucket.name
-#}
+resource "google_storage_bucket_object" "content_folder" {
+  name          = "StopPlace/"
+  content       = "Not really a directory, but it's empty."
+  bucket        = google_storage_bucket.storage_bucket.name
+}
 
 #Create pubsub topics and subscriptions
 # pubsub topics ChouetteStopPlaceSyncQueue, ChouetteStopPlaceDeleteQueue
